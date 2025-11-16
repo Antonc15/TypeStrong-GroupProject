004c61cb30744508cabd1d658bccc3e8efa11c9b
Angelica  Ballin <angelicaballin@gmail.com>
2025-11-15 22:04:16 -0800

    Allowed users to specify what type of literal to check string against

--- LiteralChecker.py
+++ LiteralChecker.py
@@ -3,6 +3,7 @@ from hexInt import state6
 from octInt import state11
 from float import state16
 
+#Method to combine all decimal, hexadecimal, octal, and float methods(NFAs) into one singular method(NFA).
 def state0(word, index):
     if state1(word, index):
         return True
@@ -14,31 +15,73 @@ def state0(word, index):
         return True
     return False
 
-choice = input("Do you want to: " + "\n (1) Enter a File \n (2) Enter a single string \n")
-if choice == "1":
-    fileName = input("Enter file name: ")
-    # Opens and takes content from file.
-    try:
-        with open(fileName, "r") as file:
-            # Reads the lines in a file and converts to an array.
-            words = file.read().splitlines()
-    except FileNotFoundError:
-        print("File not found")
+while True:
+    #Asks users what input they want to enter.
+    choice = input("Do you want to: " + "\n (1) Enter a File \n (2) Enter a single string " +
+        "\n (3) Exit the program. \n")
 
-    # Iterates through array and checks if the input is accepted or rejected.
-    for line in words:
-        word = line.split(" ")
-        if state0(word[0], 0):
-            print(f"Accepted {word[0]}")
+    if choice == "1":
+        fileName = input("Enter file name: ")
+        # Opens and takes content from file.
+        try:
+            with open(fileName, "r") as file:
+                # Reads the lines in a file and converts to an array.
+                words = file.read().splitlines()
+        except FileNotFoundError:
+            print("File not found")
+
+        # Iterates through array and checks if the input is accepted or rejected.
+        for line in words:
+            word = line.split(" ")
+            if state0(word[0], 0):
+                print(f"Accepted {word[0]}")
+            else:
+                print(f"Rejected {word[0]}")
+
+    elif choice == "2":
+        #Asks users what type of literal they would like to check.
+        choice2 = input("Do you want to check if:  " + "\n (1) String is a decimal integer literal." +
+                        "\n (2) String is a hexadecimal integer literal." + "\n (3) String is a octal integer literal." +
+                        "\n (4) String is a float literal." + "\n (5) Any python Literal. \n")
+
+        #Asks users to enter a string to check.
+        string = input("Enter string to check: ")
+
+        #Decimal Literal
+        if choice2 == "1":
+            if state1(string, 0):
+                print(f"Accepted decimal literal {string}")
+            else:
+                print(f"Rejected decimal literal{string}")
+        #Hexadecimal Literal
+        elif choice2 == "2":
+            if state6(string, 0):
+                print(f"Accepted hexadecimal literal {string}")
+            else:
+                print(f"Rejected hexadecimal literal {string}")
+        #Octal Literal
+        elif choice2 == "3":
+            if state11(string, 0):
+                print(f"Accepted octal literal {string}")
+            else:
+                print(f"Rejected octal literal {string}")
+        #Float Literal
+        elif choice2 == "4":
+            if state16(string, 0):
+                print(f"Accepted float literal {string}")
+            else:
+                print(f"Rejected float literal {string}")
+        #Any literal.
+        elif choice2 == "5":
+            if state0(string, 0):
+                print(f"Accepted literal {string}")
+            else:
+                print(f"Rejected literal {string}")
         else:
-            print(f"Rejected {word[0]}")
-
-elif choice == "2":
-    string = input("Enter string to check: ")
-    if state0(string, 0):
-        print(f"Accepted {string}")
+            print("Invalid input")
+    elif choice == "3":
+        break
     else:
-        print(f"Rejected {string}")
+        print("Enter a valid choice")
 
-else:
-    print("Enter a valid choice")
+    print("-----")
