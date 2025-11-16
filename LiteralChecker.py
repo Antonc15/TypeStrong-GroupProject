aaab0666b3716fc55a92aefabf0bb4b74ae0f01b
Angelica  Ballin <angelicaballin@gmail.com>
2025-11-15 21:23:24 -0800

    Added option for user specified files and input strings.

--- LiteralChecker.py
+++ LiteralChecker.py
@@ -14,17 +14,31 @@ def state0(word, index):
         return True
     return False
 
-#Opens and takes content from file.
-try:
-    with open("in_ans.txt", "r") as file:
-        #Reads the lines in a file and converts to an array.
-        words = file.read().splitlines()
-except FileNotFoundError:
-    print("File not found")
+choice = input("Do you want to: " + "\n (1) Enter a File \n (2) Enter a single string \n")
+if choice == "1":
+    fileName = input("Enter file name: ")
+    # Opens and takes content from file.
+    try:
+        with open(fileName, "r") as file:
+            # Reads the lines in a file and converts to an array.
+            words = file.read().splitlines()
+    except FileNotFoundError:
+        print("File not found")
 
-#Iterates through array and checks if the input is accepted or rejected.
-for word in words:
-    if state0(word, 0):
-        print(f"Accepted {word}")
+    # Iterates through array and checks if the input is accepted or rejected.
+    for line in words:
+        word = line.split(" ")
+        if state0(word[0], 0):
+            print(f"Accepted {word[0]}")
+        else:
+            print(f"Rejected {word[0]}")
+
+elif choice == "2":
+    string = input("Enter string to check: ")
+    if state0(string, 0):
+        print(f"Accepted {string}")
     else:
-        print(f"Rejected {word}")
+        print(f"Rejected {string}")
+
+else:
+    print("Enter a valid choice")
